a257662cbe198c5510c9a7aa1049049eb2d52e57
vaishnavi paturkar <vaishanavipaturkar162@gmail.com>
2026-02-22 22:03:05 +0530

    events updated

--- src/routes/events.ts
+++ src/routes/events.ts
@@ -1,10 +1,36 @@
+// import express from "express";
+// import { createEvent, getEvents } from "../controllers/eventController";
+// import { authenticate, authorizeAdmin } from "../middlewares/auths";
+// import EventModel from "../models/Event";
+// const router = express.Router();
+
+// router.get("/", getEvents);
+// router.post("/", authenticate, authorizeAdmin, createEvent);
+// router.delete("/:id", authenticate, authorizeAdmin, async (req, res) => {
+//   await Event.findByIdAndDelete(req.params.id);
+//   res.json({ message: "Event deleted" });
+// });
+
+// export default router;
 import express from "express";
 import { createEvent, getEvents } from "../controllers/eventController";
 import { authenticate, authorizeAdmin } from "../middlewares/auths";
+import EventModel from "../models/Event"; // ✅ RENAMED
 
 const router = express.Router();
 
 router.get("/", getEvents);
+
 router.post("/", authenticate, authorizeAdmin, createEvent);
+
+router.delete(
+  "/:id",
+  authenticate,
+  authorizeAdmin,
+  async (req, res) => {
+    await EventModel.findByIdAndDelete(req.params.id);
+    res.json({ message: "Event deleted" });
+  }
+);
 
 export default router;
